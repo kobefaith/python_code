52bef9da8e441cc61b3b8bc75e0efc75d5d368af
fei zhou <kobefaith@163.com>
2016-04-10 23:20:48 +0800

    add the change

--- Flask_oauth/client.py
+++ Flask_oauth/client.py
@@ -19,10 +19,35 @@ request_token_url=None,
 access_token_url='http://127.0.0.1:5000/oauth/token',
 authorize_url='http://127.0.0.1:5000/oauth/authorize'
 )
+@app.route('/')
+def index():
+   if 'remote_oauth' in session:
+        resp = remote.get('me')
+        return jsonify(resp.data)
+   next_url = request.args.get('next') or request.referrer or None
+   return remote.authorize(
+        callback=url_for('authorized',next=next_url,_external=True)
+   )
+@app.route('/authorized')
+def authorized():
+    resp = remote.authorized_response()
+    if resp is None:
+        return 'Access denied :reason=%s error=%s' %(
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    print resp
+    session['remote_oauth'] = (resp['access_token'],'')
+    return jsonify(oauth_token=resp['access_token'])
+@remote.tokengetter
+def get_oauth_token():
+    return session.get('remote_oauth')
 
-
-
-
+if __name__=='__main__':
+    import os
+    os.environ['DEBUG'] = 'true'
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = 'true'
+    app.run(host='localhost',port=8000)
 
 
 
